8dd9f6ddb0026af3057f0ba8d64ac7ae1470b9cd
GBvaillant <vaillantgabriel@gmail.com>
2023-11-06 11:06:05 -0300

    Mod: Arrumando Cors(2)

--- crudphp/excluir.php
+++ crudphp/excluir.php
@@ -1,6 +1,11 @@
 <?php 
 require 'config.php';
 
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *"); // Permite qualquer origem
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS"); // Métodos HTTP permitidos
+header("Access-Control-Allow-Headers: Content-Type, Authorization"); // Cabeçalhos personalizados permitidos
+
 $id = filter_input(INPUT_GET, 'id');
 
 if($id) { 
